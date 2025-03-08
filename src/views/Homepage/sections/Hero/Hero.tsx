fcfac76701a257453bd17a84828d3c3229cb4e27
sebasegura <sebastian.segura@strike.sh>
2025-03-08 13:41:48 -0300

    feat: update hero

--- src/views/Homepage/sections/Hero/Hero.tsx
+++ src/views/Homepage/sections/Hero/Hero.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
-import Brands from "../../components/Brands";
 import hero from "./assets/hero.png";
 
 const Hero = () => {
   return (
     <div className="relative min-h-[600px] lg:min-h-[800px] container mx-auto sm:px-4 pt-24 md:pt-32 lg:pt-48 flex flex-col items-center justify-start text-center sm:items-start sm:text-left">
       {/* <div className="flex flex-col items-center "> */}
-      <h1 className="font-h1 mb-4 lg:mb-14">Vení a Day</h1>
-      <h2 className="font-h5 lg:my-2 max-w-xs sm:max-w-sm lg:max-w-xl mx-auto sm:ml-0">
-        Encontrá la mas amplia variedad de tratamientos estéticos y maquinaria
-        con teconología de punta.
+      <h1 className="font-h1 mb-4 lg:mb-14 max-w-[900px]">
+        Lanzamos nuestro ecommerce!
+      </h1>
+      <h2 className="font-h5 lg:my-2 max-w-xs sm:max-w-sm lg:max-w-xl mx-auto sm:ml-0 font-body text-2xl font-semibold">
+        Encontrá la mas amplia variedad de productos de cuidado dermatólogico y
+        estético para tu piel y tu cuerpo.
       </h2>
       <div className="flex flex-col items-center sm:flex-row sm:items-baseline">
-        <Link href="/servicios/" passHref>
-          <a className="btn-primary mt-6 sm:mr-6">Ver servicios</a>
+        <Link href="https://dayspa.mitiendanube.com/productos" passHref>
+          <a className="btn-primary font-serif font-bold font-uppercase mt-6 sm:mr-6">
+            Ver productos
+          </a>
         </Link>
-        <Link
-          href="https://api.whatsapp.com/send?phone=5493876831581&text=Hola!%20Estaba%20viendo%20la%20web%20de%20day%20est%C3%A9tica"
-          passHref
-        >
-          <a className="text-link mt-2">Quiero alquilar</a>
+        <Link href="/servicios/" passHref>
+          <a className="text-link font-serif text-dark-900 mt-2">
+            Ver servicios
+          </a>
         </Link>
       </div>
       {/* </div> */}
